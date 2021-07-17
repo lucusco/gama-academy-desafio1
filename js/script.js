ab62ff4ea229f6daedf63c81f9fa68e0907d3f23
Luis <lu.cusco@gmail.com>
2021-07-17 14:12:07 -0300

    Folha de estilos e script

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	let nome = document.getElementById("nome").value;
+	let email = document.getElementById("email").value;
+	
+	if (nome == '' || email == '') {
+		let campoEmBranco = (nome == '') ? 'nome' : 'email';
+		mensagemRetorno('warning', `Ops, o campo com seu ${campoEmBranco} está em branco.`);
+		return;
+	}
+
+	let dadosInseridos = {
+		nome: nome,
+		email: email
+	};
+	localStorage.setItem('dataForm' ,JSON.stringify(dadosInseridos));
+	mensagemRetorno('success', "Show, cadastrado com sucesso!!!");
+
+	document.getElementById("nome").value = '';
+	document.getElementById("email").value = '';
+});
+
+function mensagemRetorno(tipo, msg) {
+	Swal.fire({
+		position: 'center',
+		icon: tipo,
+		title: '',
+		text: msg,
+		showConfirmButton: false,
+		timer: 3000,
+		timerProgressBar: true
+	} );
+
+}
